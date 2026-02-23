c7f0d5ab4de462d0e2ed7e37c74e00f0581b62e7
SHIa12l0000000 <goooogleee73@gmail.com>
2026-02-23 23:29:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const heroCarousel = document.querySelector("#heroCarousel");
+
+  if (heroCarousel) {
+    new bootstrap.Carousel(heroCarousel, {
+      interval: 5000,
+      ride: "carousel",
+      pause: false,
+      wrap: true
+    });
+  }
+});
